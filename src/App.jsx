6f195b1bfadb2a7d4df50e643a899692cef0870e
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2022-12-25 12:54:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,20 @@ import { OrbitControls, PerspectiveCamera, Stars,Text3D, KeyboardControls, Text,
 import * as THREE from "three"
 import './App.css'
 
-
-
-
 //Import components
-import Planes from '/src/Planes.jsx';
-import Floor from '/src/OtherParts/Floor';
-import {Texts} from '/src/OtherParts/Texts';
-import Loader from '/src/OtherParts/Loader';
-import ExperienceSection from "/src/ExperienceSection.jsx"
-// import Project from "/src/ProjectsSection.jsx"
-import ProjectsSection from '/src/ProjectSection.jsx';
-// import EducationSection from './assets/EducationSection';
 import Roads from '/src/OtherParts/Roads';
+import Loader from '/src/OtherParts/Loader';
+
+import ExperienceSection from "/src/ExperienceSection.jsx"
+import ProjectsSection from '/src/ProjectSection.jsx';
 import MyRoomSection from '/src/MyRoomSection.jsx';
 import WaitingSection from '/src/WaitingSection';
-// import Decorations from './assets/Decorations';
-// import Car from './assets/Car';
+
+// import Planes from '/src/Planes.jsx';
+// import Floor from '/src/OtherParts/Floor';
+// import {Texts} from '/src/OtherParts/Texts';
+
+
 
 
 
@@ -33,37 +30,23 @@ function App() {
 
       <Suspense fallback={<Loader/>}> 
         
-      <PerspectiveCamera makeDefault fov={40} zoom={1} rotation={[0,1*Math.PI,0]} position={[0, 50, -50]}/>
-      {/* <primitive object={new THREE.AxesHelper(2)}/> */}
+        <PerspectiveCamera makeDefault fov={60} zoom={1} rotation={[0,1*Math.PI,0]} position={[1, 10, 10]}/>
+        {/* <primitive object={new THREE.AxesHelper(2)}/> */}
 
-      {/* <Planes/> */}
-      {/* <Car/> */}
-      <Texts/>
-      {/* <Text className="hide" depthOffset={20}  fillOpacity={1} position={[0,5,0]}>Hello</Text> */}
+        <ExperienceSection/>
+        <ProjectsSection/>
+        <MyRoomSection/>
+        <WaitingSection/>
 
-      {/* <Floor/> */}
-      {/* <Decorations/> */}
 
-    {/* Lake*/}
-    {/* <primitive object={lake} rotation={[0,0,0]} position={[8.1,-1.4,-8.1]} /> */}
-      
-      <ExperienceSection/>
-      <ProjectsSection/>
-      {/* <EducationSection/> */}
+        <Roads/>
 
-      <MyRoomSection/>
-      <WaitingSection/>
+        <ambientLight/>
+        <Stars/>
+        <OrbitControls/>
 
-      <Roads/>
-
-      <ambientLight/>
-      <Stars/>
-      <OrbitControls/>
-
-      <pointLight position={[7,0,0]}/>
+        <pointLight position={[0,10, 0]}/>
       </Suspense>
-
-      {/* </KeyboardControls> */}
     </Canvas>
   )
 }
