d2e862ec9b4a5ab5d2414302613ca36d40f311f8
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-01-08 19:59:53 +0200

    Add tip

--- src/OtherParts/Loader.jsx
+++ src/OtherParts/Loader.jsx
@@ -19,7 +19,8 @@ export default function Loader(){
     </div>
 
     <div className={'divTip'}>
-        <h3>Tip: Don't switch sections too fast, it may cause lag.</h3>
+        <h3>Tip: 1.Your phone may be too weak to load the server, use a PC or tablet.</h3> <br></br>
+        <h3>2. Don't switch sections too fast, it may cause lag.</h3>
     </div>
     </Html>
 }
